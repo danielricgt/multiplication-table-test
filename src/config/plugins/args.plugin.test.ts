c4661d20f9bbf2d0b8774f17e10bc1a3416387ce
Daniel galvan <danielricardogt@gmail.com>
2025-06-22 22:16:57 -0500

    :test: arg plugin test part II

--- src/config/plugins/args.plugin.test.ts
+++ src/config/plugins/args.plugin.test.ts
@@ -5,16 +5,42 @@ const runCommand = async (args: string[]) => {
 };
 
 describe("Test on args.plugin", () => {
+
+  const originalArgv = process.argv;
+  beforeEach(() => {
+    process.argv = originalArgv;
+    jest.resetModules();
+  });
+
   test("should return default values ", async () => {
     const argv = await runCommand(["-b", "5"]);
     //   console.log(argv);
 
+    expect(argv).toEqual(
+      expect.objectContaining({
+        b: 5,
+        l: 10,
+        s: false,
+        n: "multiplication-table",
+        d: "outputs",
+      })
+    );
+  });
+
+  test("should return with coustom values", async ()  => {
+
+    const argv = await runCommand(["-b","7","-s","-n 7","-d files"])
+    console.log(argv);
     expect(argv).toEqual(expect.objectContaining({
-      b: 5,
+      b: 7,
+      base: 7,
+      s: true,
+      show: true,
+      n: ' 7',
+      d: ' files',
       l: 10,
-      s: false,
-      n: "multiplication-table",
-      d: "outputs",
+      limit: 10,
     }));
-  }); 
+
+  });
 });
