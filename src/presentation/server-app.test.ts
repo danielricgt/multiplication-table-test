5bce190b2eb28d140271c5a09be513cb8ee8a072
Daniel galvan <danielricardogt@gmail.com>
2025-06-23 23:40:00 -0500

    :test: arg plugin test part III

---
+++ src/presentation/server-app.test.ts
@@ -0,0 +1,51 @@
+import { createInflate } from "zlib";
+import { CreateTable } from "../domain/use-cases/create-table.use-case";
+import { SaveFile } from "../domain/use-cases/save-file.use-case";
+import { ServerApp } from "./server-app";
+
+describe('server app test', () => { 
+
+    const options = {
+        base:3,
+        limit:10,
+        showTable:false,
+        fileDestination: 'trest-destination' ,
+        fileName:'test-file-namne',
+    };
+test('should create server App instance', () => {
+    
+    const serverApp = new ServerApp();
+    expect(serverApp).toBeInstanceOf(ServerApp );
+    expect(typeof ServerApp.run).toBe('function');
+
+});
+
+
+test('should run server app with options ', () => {
+
+
+    const logSpy = jest.spyOn(console, 'log');
+    const createTableSpy = jest.spyOn(CreateTable.prototype, 'execute');
+    const saveFileSpy = jest.spyOn(SaveFile.prototype, 'execute');
+
+
+    ServerApp.run(options);
+    expect(logSpy).toHaveBeenCalledTimes(2);
+    expect(logSpy).toHaveBeenCalledWith('Server running...');
+    expect(logSpy).toHaveBeenLastCalledWith('File created!');
+
+    expect(createTableSpy).toHaveBeenCalledTimes (1);
+    expect(createTableSpy).toHaveBeenCalledWith({base: options.base, limit: options.limit});
+
+    expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenCalledWith({
+        fileContent: expect.any(String),
+        fileDestination: options.fileDestination,
+        fileName: options.fileName,
+    });
+
+});
+
+
+
+ });
